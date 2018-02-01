442c4317561d7f16d6af9becd7c699ac7d2e3185
Jaxel Rojas <jaxelr@gmail.com>
2018-02-01 16:19:36 -0400

    Added AspnetCore.HealthChecks Added to gitignore .vscode Added to gitignore publishprofiles Added dockerfile Added appsettings.json

--- src/Program.cs
+++ src/Program.cs
@@ -1,20 +1,27 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using System.IO;
 
 namespace Nancy.Template.WebService
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+        public static void Main(string[] args) => BuildWebHost(args).Run();
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseHealthChecks("/healthcheck")
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    var env = builderContext.HostingEnvironment;
+
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables();
+                })
                 .UseIISIntegration()
                 .Build();
     }
--- src/Startup.cs
+++ src/Startup.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.HealthChecks;
 using Nancy.Owin;
+using System.Threading.Tasks;
 
 namespace Nancy.Template.WebService
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
-        {
-        }
+            =>
+            //HealthChecks
+            services.AddHealthChecks(checks =>
+            {
+                checks.AddValueTaskCheck("HTTP Endpoint", () => new
+                    ValueTask<IHealthCheckResult>(HealthCheckResult.Healthy("Ok")));
+            });
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
