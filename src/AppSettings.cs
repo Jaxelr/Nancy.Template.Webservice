f200b498a5b2de796e92bf2353e98638b89cc538
Jaxel Rojas <jaxelr@gmail.com>
2018-02-05 14:03:30 -0400

    Updated AppSettings.json and AppSettings class to bind the values of the config. Added autowire to the dependency of the Startup class.

---
+++ src/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace Api
+{
+    public class AppSettings
+    {
+        public Cache Cache { get; set; }
+        public ErrorMessages ErrorMessages { get; set; }
+    }
+
+    public class Cache
+    {
+        public bool CacheEnabled { get; set; }
+        public int CacheTimespan { get; set; }
+    }
+
+    public class ErrorMessages
+    {
+        public string ModelBindingFailure { get; set; }
+    }
+}
--- src/Startup.cs
+++ src/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.HealthChecks;
 using Nancy.Owin;
@@ -9,14 +10,30 @@ namespace Nancy.Template.WebService
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
-            =>
+        {
             //HealthChecks
             services.AddHealthChecks(checks =>
             {
                 checks.AddValueTaskCheck("HTTP Endpoint", () => new
                     ValueTask<IHealthCheckResult>(HealthCheckResult.Healthy("Ok")));
             });
+
+            //Extract the AppSettings information from the
+            var appSettings = new Api.AppSettings();
+
+            Configuration.GetSection("AppSettings")
+                .Bind(appSettings);
+
+            services.AddSingleton(appSettings);
+        }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
