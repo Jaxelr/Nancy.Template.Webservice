a43090189bab9f1a08161e5538f8818325a5e12b
Jaxel Rojas <jaxelr@gmail.com>
2019-08-11 09:04:43 -0400

    refactor: removing unneeded ref to newtonsoft.json

--- Content/src/Models/Entities/Hello.cs
+++ Content/src/Models/Entities/Hello.cs
@@ -1,8 +1,5 @@
-﻿using Newtonsoft.Json;
-
-namespace Nancy.Template.WebService.Models.Entities
+﻿namespace Nancy.Template.WebService.Models.Entities
 {
-    [JsonObject(MemberSerialization.OptOut)]
     public class Hello
     {
         public string Name { get; set; }
--- Content/src/Models/Operations/Hello.cs
+++ Content/src/Models/Operations/Hello.cs
@@ -1,15 +1,12 @@
 ﻿using Nancy.Template.WebService.Models.Entities;
-using Newtonsoft.Json;
 
 namespace Nancy.Template.WebService.Models.Operations
 {
-    [JsonObject(MemberSerialization.OptOut)]
     public class HelloRequest
     {
         public string Name { get; set; }
     }
 
-    [JsonObject(MemberSerialization.OptOut)]
     public class HelloResponse
     {
         public Hello Response { get; set; }
