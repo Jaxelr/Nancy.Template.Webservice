b0492c23e7fdbaae1a5ab52aed78fa0d69dd61ec
Jaxel Rojas <jaxelr@gmail.com>
2020-05-08 09:44:06 -0400

    refactor: splitted metadata class from module class into separate files

--- Content/src/Modules/HelloModule.cs
+++ Content/src/Modules/HelloModule.cs
@@ -1,8 +1,5 @@
 ﻿using System.Diagnostics;
 using Nancy;
-using Nancy.Metadata.Modules;
-using Nancy.Metadata.OpenApi.Core;
-using Nancy.Metadata.OpenApi.Fluent;
 using Nancy.Template.WebService.Extensions;
 using Nancy.Template.WebService.Models.Entities;
 using Nancy.Template.WebService.Models.Operations;
@@ -10,31 +7,19 @@ using Nancy.Template.WebService.Repositories;
 
 namespace Nancy.Template.WebService.Modules
 {
-    public class SampleMetadataModule : MetadataModule<OpenApiRouteMetadata>
-    {
-        public SampleMetadataModule()
-        {
-            Describe[nameof(Hello)] = desc => new OpenApiRouteMetadata(desc)
-                .With(i => i.WithResponseModel(HttpStatusCode.OK, typeof(HelloResponse), "Hello Response")
-                .WithDescription("This operation returns the hello world message", tags: new string[] { "Hello" })
-                .WithResponseModel(HttpStatusCode.BadRequest, typeof(string), "Failed Validation Response")
-                .WithRequestParameter("name", description: "A string to return as part of hello world", loc: Loc.Query, type: typeof(string)));
-        }
-    }
-
-    public class SampleModule : NancyModule
+    public class HelloModule : NancyModule
     {
         private readonly Stopwatch watch;
         private readonly IHelloRepository repo;
 
-        public SampleModule(Stopwatch watch, IHelloRepository repo) : base("api")
+        public HelloModule(Stopwatch watch, IHelloRepository repo) : base("api")
         {
             this.repo = repo;
 
             this.watch = watch;
             this.watch.Restart();
 
-            this.GetHandler<HelloRequest, HelloResponse>(nameof(Hello), HelloOp);
+            this.Get<HelloRequest, HelloResponse>(nameof(Hello), HelloOp);
         }
 
         public HelloResponse HelloOp(HelloRequest user) => new HelloResponse { Response = repo.SayHello(user.Name) };
---
+++ Content/src/Modules/Metadata/HelloMetadataModule.cs
@@ -0,0 +1,20 @@
+﻿using Nancy.Metadata.Modules;
+using Nancy.Metadata.OpenApi.Core;
+using Nancy.Metadata.OpenApi.Fluent;
+using Nancy.Template.WebService.Models.Entities;
+using Nancy.Template.WebService.Models.Operations;
+
+namespace Nancy.Template.WebService.Modules.Metadata
+{
+    public class HelloMetadataModule : MetadataModule<OpenApiRouteMetadata>
+    {
+        public HelloMetadataModule()
+        {
+            Describe[nameof(Hello)] = desc => new OpenApiRouteMetadata(desc)
+                .With(i => i.WithResponseModel(HttpStatusCode.OK, typeof(HelloResponse), "Hello Response")
+                .WithDescription("This operation returns the hello world message", tags: new string[] { "Hello" })
+                .WithResponseModel(HttpStatusCode.BadRequest, typeof(string), "Failed Validation Response")
+                .WithRequestParameter("name", description: "A string to return as part of hello world", loc: Loc.Query, type: typeof(string)));
+        }
+    }
+}
